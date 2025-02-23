7abfe40acb6db292a2634b5974268cbd26357503
Peng Yu <peng.yu@gmail.com>
2025-02-22 23:46:14 -0500

    run build

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,6 +1,9 @@
+import cesium from 'vite-plugin-cesium';
 export default {
+  plugins: [cesium()],
   build: {
     sourcemap: true,
+    emptyOutDir: true,
     outDir: "../dist",
   },
   publicDir: "./assets",
